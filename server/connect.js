f365c2391893f4e99eea08ad527f71f39a5d1bf8
Biswajeet Dash <90475327+ScOuT408@users.noreply.github.com>
2023-01-19 23:08:29 +0530

    bugs fixed

--- server/connect.js
+++ server/connect.js
@@ -3,16 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-// });
-
 export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "crud_books",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
+
